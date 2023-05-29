450822b4cff6cb2b8c87b53a3612686a5577fabc
fasyaaa <muhammad.a.f.2210@gmail.com>
2023-05-29 15:18:04 +0700

    21

---
+++ ProblemMultiWaris/ProblemMultiWaris.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class orang {
+public : 
+	int umur;
+	
+	orang(int pUmur) :
+		umur(pUmur) {
+		cout << " orang dibuat dengan umur " << umur << "\n" << endl;
+	}
+};
+
+class pekerja : public orang {
+public:
+	
+	pekerja(int pUmur) :
+		orang(pUmur) {
+		cout << "Pekerja dibuat \n" << endl;
+	}
+};
+
